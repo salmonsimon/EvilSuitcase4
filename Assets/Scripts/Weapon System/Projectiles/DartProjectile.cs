0aed5836d22ff9d37d4ea7a6bf488ebb8b2f7e66
salmonsimon <38538239+salmonsimon@users.noreply.github.com>
2023-02-17 16:32:04 -0300

    Rocket Launcher (#9)

--- Assets/Scripts/Weapon System/Projectiles/DartProjectile.cs
+++ Assets/Scripts/Weapon System/Projectiles/DartProjectile.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class DartProjectile : Projectile
 {
+    [SerializeField] private DamageConfigurationScriptableObject damageConfig;
+    public DamageConfigurationScriptableObject DamageConfig { get { return damageConfig; } set { damageConfig = value; } }
+
     protected override void OnCollisionEnter(Collision collision)
     {
         base.OnCollisionEnter(collision);
 
         transform.SetParent(collision.collider.transform);
+
+        if (collision.collider.TryGetComponent(out Damageable damageable))
+            damageable.ReceiveDamage(damageConfig.GetDamage(distanceTraveled));
+
+        Disable();
     }
 }
